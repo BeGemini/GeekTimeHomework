0a030e1016178157b855f9c8872c2828f12fb744
BeGemini <cutetoofat@outlook.com>
2021-07-25 22:13:38 +0800

    homework of second week

---
+++ src/Persistence/EmailAccountDao.go
@@ -0,0 +1,33 @@
+package Persistence
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type EmailAccount struct {
+	ID int
+	Email string
+	EmailPwd string
+	EmailHost string
+	EmailPort string
+}
+
+func GetEmailAccountList() ([]EmailAccount,error){
+	result := make([]EmailAccount,0)
+	db,_:=sql.Open("mysql","root:Hhl@0048@(127.0.0.1:3306)/TicketsMem")
+	err:=db.Ping()
+	if err!=nil{
+		fmt.Println("Error occured during connecting db.")
+	}
+	defer db.Close()
+	rows,_:= db.Query("SELECT * FROM EmailAccount")
+	var ID int
+	var Email string
+	for rows.Next(){
+		rows.Scan(&ID,&Email)
+		result = append(result,EmailAccount{ID: ID,Email: Email})
+	}
+	return result,rows.Err()
+}
---
+++ src/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"GeekTimeHomework/src/Persistence"
+	"fmt"
+)
+
+func main(){
+	emails,err := Persistence.GetEmailAccountList()
+	if err!=nil{
+		for _,email := range emails{
+			fmt.Println(email.ID,email.Email)
+		}
+	}
+}
