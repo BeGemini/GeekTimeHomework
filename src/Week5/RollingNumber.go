25e846fb2f63bb5bb7adf3d3e11dbc5f400394c7
BeGemini <cutetoofat@outlook.com>
2021-08-20 09:20:25 +0800

    add function increase and getsum

--- src/Week5/RollingNumber.go
+++ src/Week5/RollingNumber.go
@@ -1,4 +1,42 @@
 package Week5
 
+import "time"
+
 type RollingNumber struct {
+	startMS      int64
+	cap          int
+	perBucketCap int
+	head         int
+	tail         int
+	buckets      []*bucket
+}
+
+type Event int
+
+const (
+	Success Event = iota
+	Failure
+	Timeout
+	Rejection
+)
+
+func NewRollingNmber(cap int, perBucketCap int) *RollingNumber {
+	start := time.Now().UnixNano() / 1e6
+	return &RollingNumber{
+		startMS:      start,
+		cap:          cap,
+		perBucketCap: perBucketCap,
+		head:         0,
+		tail:         0,
+		buckets:      make([]*bucket, cap),
+	}
+}
+
+func (e *RollingNumber) Increase(event Event) {
+	currentTime := time.Now().UnixNano() / 1e6
+
+}
+
+func (e *RollingNumber) GetRollingSum() {
+
 }
--- src/Week5/bucket.go
+++ src/Week5/bucket.go
@@ -1,23 +1,10 @@
 package Week5
 
-const (
-	success = iota
-	failure
-	timeout
-	rejection
-)
-
 type bucket struct {
 	success     int64
 	failure     int64
 	timeout     int64
 	rejection   int64
 	windowStart int64
-}
-
-func newBucket(time int64) *bucket {
-	_bucket := &bucket{
-		windowStart: time,
-	}
-	return _bucket
+	cap         int // how long the bucket mean
 }
